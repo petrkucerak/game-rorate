be288cbe96f40f51475c58dd980c5d842dba1bc8
petrkucerak <petr@khome.cz>
2022-07-01 11:30:44 +0200

    jidlo boost

--- pages/points/jidlo-a-potraviny.js
+++ pages/points/jidlo-a-potraviny.js
@@ -10,8 +10,8 @@ export default function JidloAPotraviny() {
         name={"Jídlo a potraviny"}
         describtion="Bez jídla a stravy nikdo nevydrží dlouho. I když máte-li zájem, můžete to klidně zkusit. Pokud se ale chcete pořádně připravit, musíte odpovědět na následující otázku."
         coords="50.0758742N, 14.4168725E"
-        question="Na zemi se píše, že ve 42. tu byla honička. Jaká kombinace barev zobrazuje tento rok?"
-        answare="černobílá"
+        question="Mé jméno je Gorazd. Co jsem?"
+        answare="biskup"
         odpoved="#adAFS5A*"
       />
       <Footer />
