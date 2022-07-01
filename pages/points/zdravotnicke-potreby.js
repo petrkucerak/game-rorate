fc67cb3185c532103c220a4d5d6cb397f1aa1e87
petrkucerak <petr@khome.cz>
2022-07-01 11:03:15 +0200

    Update text

--- pages/points/zdravotnicke-potreby.js
+++ pages/points/zdravotnicke-potreby.js
@@ -10,7 +10,7 @@ export default function ZdravotnickePotreby() {
         name={"Zdravotnické potřeby"}
         describtion="Už od dob dávných platí, že vědět, znamená mnoho. A v medicíně především. Vždyť například s první pitvou bylo objeveno tolik nových věcí. Chcete-li získat zdravotnické potřeby, na kterých zajisté bude záviset vaše přežití, musíte správně odpovědět na otázku."
         coords="50.0841581N, 14.4083333E"
-        question="Pevné jak ze železa jsme. Tvoříme a chodíme a často ťukáme. Kolik nás je?"
+        question="Pevné jak ze železa jsme. Tvoříme, chodíme a symbol manželství na nás navlékneš. Kolik nás je?"
         answare="60"
         odpoved="#sdfDS58*"
       />
